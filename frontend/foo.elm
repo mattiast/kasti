62d4aca52299b7f29bb98c98b784aae65be8b76c
Matti Astrand <mattiastr@gmail.com>
2017-07-11 12:27:48 +0100

    audio element

--- frontend/foo.elm
+++ frontend/foo.elm
@@ -1,9 +1,12 @@
 -- Read more about this program in the official Elm guide:
 -- https://guide.elm-lang.org/architecture/user_input/buttons.html
 
-import Html exposing (beginnerProgram, div, button, text)
+import Html exposing (beginnerProgram, div, button, text, audio, br)
+import Html.Attributes exposing (src, controls)
 import Html.Events exposing (onClick)
 
+audioUrl : String
+audioUrl = "http://feeds.soundcloud.com/stream/322754602-stack-exchange-stack-overflow-podcast-109.mp3"
 
 main =
   beginnerProgram { model = 0, view = view, update = update }
@@ -14,6 +17,8 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , br [] []
+    , audio [ src audioUrl, controls True ] []
     ]
 
 

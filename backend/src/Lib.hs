05582552c24af9e07bdbec6b0d9c684aea87b4bd
Matti Astrand <mattiastr@gmail.com>
2017-07-11 15:41:31 +0100

    serve static html

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -7,6 +7,9 @@ import Data.Monoid (mconcat)
 
 someFunc :: IO ()
 someFunc = scotty 3000 $ do
+    get "/play" $ do
+        setHeader "Content-Type" "text/html; charset=utf-8"
+        file "index.html"
     get "/:word" $ do
         beam <- param "word"
         html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]

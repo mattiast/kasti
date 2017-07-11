bf1dea5ead1eebc7e6e1d73e81e7614127541764
Matti Astrand <mattiastr@gmail.com>
2017-07-11 14:59:18 +0100

    silly web server

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
+import Web.Scotty
+import Data.Monoid (mconcat)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = scotty 3000 $ do
+    get "/:word" $ do
+        beam <- param "word"
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]

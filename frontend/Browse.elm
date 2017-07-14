2a9f1b6d66a49c14159038b83604cf1fbd308835
Matti Astrand <mattiastr@gmail.com>
2017-07-14 13:03:18 +0100

    pull episode list from server, show it

--- frontend/Browse.elm
+++ frontend/Browse.elm
@@ -8,8 +8,10 @@ import Http
 import Platform.Cmd as Cmd
 import Platform.Sub as Sub
 import RemoteData as RD
+import Episode as E
 
 
+main : Program Never Model Msg
 main =
     program
         { init = init
@@ -21,16 +23,27 @@ main =
 
 type alias Model =
     { feeds : RD.WebData (List Feed)
+    , episodes : RD.WebData (List E.Episode)
     }
 
 
 type Msg
     = FeedsReceive (RD.WebData (List Feed))
+    | EpMsg E.Msg
+    | AskEpList Int
+    | ReceiveEpList (RD.WebData (List E.Episode))
+
+
+type alias Feed =
+    { id : Int
+    , name : String
+    , url : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model RD.NotAsked
+    ( Model RD.NotAsked RD.NotAsked
     , RD.sendRequest getFeeds
         |> Cmd.map FeedsReceive
     )
@@ -44,33 +57,50 @@ update msg model =
             , Cmd.none
             )
 
+        EpMsg (E.Pick ep) ->
+            ( model, Cmd.none )
+
+        AskEpList feed_id ->
+            ( { model | episodes = RD.Loading }
+            , Http.get ("/episodes/" ++ toString feed_id) (D.list E.decodeEpisode)
+                |> RD.sendRequest
+                |> Cmd.map ReceiveEpList
+            )
+
+        ReceiveEpList stuff ->
+            ( { model | episodes = stuff }, Cmd.none )
+
 
 view : Model -> Html Msg
 view model =
-    case model.feeds of
+    div []
+        [ feedList model.feeds
+        , model.episodes
+            |> RD.map E.episodeList
+            |> RD.withDefault (text (toString model.episodes))
+            |> Html.map EpMsg
+        ]
+
+
+feedList : RD.WebData (List Feed) -> Html Msg
+feedList feeds =
+    case feeds of
         RD.Success fs ->
             let
                 feedItem f =
-                    li [] [ a [href f.url] [text f.name] ]
+                    li [] [ a [ onClick (AskEpList f.id) ] [ text f.name ] ]
             in
                 fs
                     |> List.map feedItem
                     |> ul []
 
         _ ->
-            text (toString model)
+            text (toString feeds)
 
 
 getFeeds : Http.Request (List Feed)
 getFeeds =
     Http.get "/feeds" (D.list decodeFeed)
-
-
-type alias Feed =
-    { id : Int
-    , name : String
-    , url : String
-    }
 
 
 decodeFeed : D.Decoder Feed
---
+++ frontend/Episode.elm
@@ -0,0 +1,52 @@
+module Episode exposing (..)
+
+import Html exposing (Html, Attribute, beginnerProgram, program, div, button, text, audio, br, ul, li, a)
+import Html.Attributes exposing (src, controls, style, href)
+import Html.Events exposing (onClick, on)
+import Json.Decode as D
+import Http
+import Platform.Cmd as Cmd
+import Platform.Sub as Sub
+import RemoteData as RD
+import Date exposing (Date)
+
+
+type alias Episode =
+    { title : String
+    , url : String
+    , date : Date
+    }
+
+
+decodeEpisode : D.Decoder Episode
+decodeEpisode =
+    D.map3 Episode
+        (D.field "title" D.string)
+        (D.field "url" D.string)
+        (D.field "date" decodeDate)
+
+
+decodeDate : D.Decoder Date
+decodeDate =
+    let
+        helper : String -> D.Decoder Date
+        helper str =
+            case Date.fromString str of
+                Ok date ->
+                    D.succeed date
+
+                Err error ->
+                    D.fail (toString error)
+    in
+        D.andThen helper D.string
+
+
+type Msg
+    = Pick Episode
+
+
+episodeList : List Episode -> Html Msg
+episodeList eps =
+    eps
+        |> List.map (\e -> li [] [ a [ onClick (Pick e) ] [ text e.title ] ])
+        |> ul []

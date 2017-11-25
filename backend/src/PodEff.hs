5d588ea972665f341a9e7fe3f243f64004f4a2db
Matti Astrand <mattiastr@gmail.com>
2017-11-25 23:01:01 +0000

    Implement pod read effects.

--- backend/src/PodEff.hs
+++ backend/src/PodEff.hs
@@ -6,13 +6,27 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module PodEff where
 import Types
 import Control.Eff
+import Database.PostgreSQL.Simple(Connection)
+import EpisodeDb
 
 data PodEff a where
-    PodRead :: EpisodeId -> PodEff Episode
+    PodGetFeedInfo :: FeedId -> PodEff (Maybe FeedInfo)
+    PodGetFeeds :: PodEff [(FeedId, FeedInfo)]
 
-getEpi :: (Member PodEff e) => EpisodeId -> Eff e Episode
-getEpi eid = send (PodRead eid)
+getFeedInfo :: (Member PodEff e) => FeedId -> Eff e (Maybe FeedInfo)
+getFeedInfo fid = send (PodGetFeedInfo fid)
+
+getFeeds :: (Member PodEff e) => Eff e [(FeedId, FeedInfo)]
+getFeeds = send PodGetFeeds
+
+runPod :: (Member IO r) => Connection -> Eff (PodEff ': r) w -> Eff r w
+runPod conn s = handle_relay return (\(x :: PodEff v) (f :: Arr r v w) -> oneQ conn x f) s
+
+oneQ :: (Member IO r) => Connection -> PodEff v -> Arr r v a -> Eff r a
+oneQ conn (PodGetFeedInfo fid) f = send (readFeed fid conn) >>= f
+oneQ conn PodGetFeeds f = send (readFeeds conn) >>= f

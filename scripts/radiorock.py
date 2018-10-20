95ac7408e3619b5d69f111070b8409592a878e7d
Matti Astrand <mattiastr@gmail.com>
2018-10-20 21:51:07 +0100

    Use other endpoint to fetch episode data

--- scripts/radiorock.py
+++ scripts/radiorock.py
@@ -1,116 +1,55 @@
-import datetime
+import datetime as dt
 import logging
-import time
-from typing import NamedTuple, Iterator, Optional
+from typing import Iterator
+from urllib.parse import quote
 
-import dryscrape
+import lxml.etree
 import psycopg2
-from episodedb import write_db, EpisodeData
+import requests
+from episodedb import EpisodeData, write_db
 
 
 log = logging.getLogger(__name__)
 
 
-class EpisodeInfo(NamedTuple):
-    title: str
-    description: str
-    date: datetime.datetime
-    link_url: str
-
-
-def get_episodes(
-    sess: dryscrape.Session, query: str, pages: int = 1
-) -> Iterator[EpisodeInfo]:
-    log.info("Opening the website")
-    sess.visit("/#!/ohjelma/%s" % query)
-    log.info("Let's increase the window size and wait a bit...")
-    sess.driver.set_viewport_size(2000, 2000)
-    time.sleep(3)
-    log.info("OK we'll scrape %d pages", pages)
-    for i in range(pages - 1):
-        log.info("Page %d...", i)
-        sess.driver.exec_script("window.scrollBy(0, 2000)")
-        time.sleep(1)
-
-    arts = sess.xpath(
-        "//article[contains(@class, 'item') and contains(@class ,'post')]"
+def fetch_episodes(
+    tag_name, tag_cat="ohjelma", page=0
+) -> Iterator[EpisodeData]:
+    url = "https://www.radiorock.fi/api/content?tagCategory={}&tagName={}&page={}".format(
+        tag_cat, quote(tag_name), page
     )
-    log.info("Found %d articles that are potential episodes", len(arts))
 
-    for a in arts:
-        [c] = a.xpath("div[@class='item-content']")
-        (title,) = c.xpath("h2/a")
-        (ep_time,) = c.xpath("div/time")
-        ep_time = datetime.datetime.fromtimestamp(int(ep_time["datetime"]) / 1000)
+    r = requests.get(url)
 
-        description = c.xpath("div/p")
-        if description:
-            description = description[0].text()
-        else:
-            description = ""
+    stuff = r.json()
 
-        ep_info = EpisodeInfo(
-            title=title.text(),
-            date=ep_time,
-            description=description,
-            link_url=title["href"],
+    for post in stuff["posts"]:
+        if "media" not in post:
+            continue
+        title = post["media"]["title"]
+        description = post["media"]["description"]
+        ts = dt.datetime.utcfromtimestamp(post["created_at_epoch"] / 1000)
+        log.info("Found episode: %s", title)
+        media_id = post["media"]["id"]
+
+        r = requests.get(
+            "https://gatling.nelonenmedia.fi/media-xml-cache?v=2&id={}".format(media_id)
         )
-        log.info("Found an episode on %s", ep_info.date)
-        yield ep_info
-
-
-def get_audio_url(sess: dryscrape.Session, link_url: str) -> Optional[str]:
-    log.info("Opening the page")
-    sess.visit(link_url)
-
-    sess.driver.wait_for(lambda: len(sess.xpath("//audio")) > 1)
-    audios = sess.xpath("//audio")
-    for a in audios:
-        a.exec_script("node.pause()")
-
-    log.info("Found %d audio tags", len(audios))
-    audios = [a for a in audios if a["data-isad"] == "false"]
-    log.info("Found %d non-ad audio tags", len(audios))
-
-    return audios[0]["src"] if audios else None
-
-
-def ep_data(ep_info: EpisodeInfo, audio_url: str) -> EpisodeData:
-    return EpisodeData(
-        title=ep_info.title,
-        description=ep_info.description,
-        date=ep_info.date,
-        audio_url=audio_url,
-    )
+        e = lxml.etree.fromstring(r.content)
+        audios = e.xpath("//AudioMediaFile")
+        if audios:
+            audio_url = audios[0].text
+            yield EpisodeData(
+                title=title, description=description, date=ts, audio_url=audio_url
+            )
 
 
 def fetch_and_write(showname, dbpath, query=None):
     if query is None:
         query = showname
-    dryscrape.start_xvfb()
-    sess = dryscrape.Session(base_url="https://www.radiorock.fi")
-    sess.set_attribute("auto_load_images", False)
-    es = get_episodes(sess, query)
-    es = list(es)
-    sess.reset()
+
+    stuff = list(fetch_episodes(query))
     conn = psycopg2.connect(dbpath)
-    c = conn.cursor()
-    c.execute("select distinct date from rrepisodes where show = %s", (showname,))
-    dates = set(date.replace(tzinfo=None) for (date,) in c)
-    stuff = []
-    for e in es:
-        if e.date in dates:
-            log.info("Date = %s already exists, skipping...", e.date)
-            continue
-        try:
-            audio_url = get_audio_url(sess, e.link_url)
-        except:
-            log.exception("Error while getting audio url for %s", e)
-            continue
-        sess.reset()
-        if audio_url:
-            data = ep_data(e, audio_url)
-            stuff.append(data)
     n = write_db(conn, stuff, showname)
     conn.close()
     return n

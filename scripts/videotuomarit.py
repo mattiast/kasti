c58872287bbe7a496236929d2e49b8ffae9d14f3
Matti Astrand <mattiastr@gmail.com>
2018-11-03 12:23:52 +0000

    Different scraper for videos

--- scripts/videotuomarit.py
+++ scripts/videotuomarit.py
@@ -2,49 +2,64 @@ import argparse
 import json
 import logging
 import sys
-import time
 from datetime import datetime
+from io import BytesIO
+from typing import Iterable, Iterator, NamedTuple
 
-import dateparser
-import dryscrape
 import PyRSS2Gen
+import requests
+from lxml import etree, html
 from update_rss import push_to_s3
 
 
 log = logging.getLogger(__name__)
 
 
-def get_episodes():
-    dryscrape.start_xvfb()
-    sess = dryscrape.Session(base_url="http://www.is.fi")
-    sess.set_attribute("auto_load_images", False)
-    sess.visit("/videotuomarit")
-    time.sleep(3)
-
-    arts = sess.xpath(
-        "//article[contains(@class,'teaser') and not(contains(@class,'is-advertorial'))]"
-    )
-    log.info("Found %d articles", len(arts))
-
-    heading = lambda a: a.xpath("a/div[contains(@class,'teaser-heading')]/h2")[0].text()
-    lnk_url = lambda a: a.xpath("a")[0].get_attr("href")
-    datef = lambda a: a.xpath("a/div[@class='details']/span[@class='updated']/time")[
-        0
-    ].get_attr("data-formatted-date")
-    s2d = lambda s: dateparser.parse(s, languages=["fi"])
-
-    stuff = [
-        {
-            "title": heading(a),
-            "url": "http://www.is.fi" + lnk_url(a),
-            "date": s2d(datef(a)),
-        }
-        for a in arts
-    ]
-    return stuff
+class Stuff(NamedTuple):
+    title: str
+    date: datetime
+    link_url: str
+    video_url: str
 
 
-def generate_rss(stuff):
+def get_episodes2() -> Iterator[Stuff]:
+    r = requests.get("https://www.is.fi/videotuomarit/")
+    doc = html.parse(BytesIO(r.content))
+    alinks = doc.xpath("//a[@name]")
+
+    for a in alinks:
+        hrefs = a.xpath("@href")
+        titles = a.xpath("div[@class='teaser-heading']/h2/text()")
+        if not hrefs or not titles:
+            continue
+        title = titles[0].strip()
+        link = f"https://www.is.fi{hrefs[0]}"
+
+        r = requests.get(link)
+        doc = html.parse(BytesIO(r.content))
+        dates = doc.xpath("//time[@itemprop='datePublished']/@content")
+        if not dates:
+            continue
+        date = datetime.fromisoformat(dates[0])
+        media_urls = doc.xpath(
+            "//div[@class='video-container']/div/@data-config-url"
+        )
+        if len(media_urls) != 1:
+            continue
+        media_url, = media_urls
+
+        r = requests.get(f"http:{media_url}")
+        d = etree.parse(BytesIO(r.content))
+
+        files = d.xpath("//HTTPMediaFile/text()")
+        if not files:
+            continue
+        video_url = str(files[0])
+        stuff = Stuff(title, date, link, video_url)
+        yield stuff
+
+
+def generate_rss(stuff: Iterable[Stuff]) -> PyRSS2Gen.RSS2:
     rss = PyRSS2Gen.RSS2(
         title="Videotuomarit",
         link="http://www.is.fi/videotuomarit",
@@ -52,10 +67,11 @@ def generate_rss(stuff):
         lastBuildDate=datetime.now(),
         items=[
             PyRSS2Gen.RSSItem(
-                title=s["title"],
-                guid=PyRSS2Gen.Guid(s["url"]),
-                link=s["url"],
-                pubDate=s["date"],
+                title=s.title,
+                guid=PyRSS2Gen.Guid(s.link_url),
+                link=s.link_url,
+                pubDate=s.date,
+                enclosure=PyRSS2Gen.Enclosure(s.video_url, 0, "video/mp4"),
             )
             for s in stuff
         ],
@@ -73,7 +89,8 @@ if __name__ == "__main__":
     with open(args.conffile) as f:
         conf = json.load(f)
 
-    stuff = get_episodes()
+    stuff = get_episodes2()
+    stuff = list(stuff)
 
     log.info("Generating RSS")
     rss = generate_rss(stuff)

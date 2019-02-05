aec4c2839223756211899f438f4df5304de9d573
Matti Astrand <mattiastr@gmail.com>
2019-02-05 19:06:56 +0000

    Elm nix files

--- frontend/default.nix
+++ frontend/default.nix
@@ -1,8 +1,25 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation rec {
-  name = "kasti-frontend";
-  env = buildEnv { name = name; paths = buildInputs; };
-  buildInputs = [
-    elmPackages.elm
-  ];
+{ nixpkgs ? <nixpkgs>
+, config ? {}
+}:
+
+with (import nixpkgs config);
+
+let
+  srcs = ./elm-srcs.nix;
+  versionsDat = ./versions.dat;
+in stdenv.mkDerivation {
+  name = "elm-app-0.1.0";
+  src = ./.;
+
+  buildInputs = [ elmPackages.elm ];
+
+  buildPhase = pkgs.elmPackages.fetchElmDeps {
+    elmPackages = import srcs;
+    inherit versionsDat;
+  };
+
+  installPhase = ''
+    elm make Browse.elm --output $out/elm.js
+  '';
 }
+
---
+++ frontend/elm-srcs.nix
@@ -0,0 +1,82 @@
+{
+
+      "krisajenkins/remotedata" = {
+        sha256 = "0sjrgpsy4aci0h6qndqx70cnfd065hlkh1rxk0lc93z22sdj4knl";
+        version = "5.0.0";
+      };
+
+      "surprisetalk/elm-bulma" = {
+        sha256 = "0dwc7jsjshxhf8sfgw8a7hi7dk68hdh0gc5ygvh43wad3x1zqhzh";
+        version = "6.1.2";
+      };
+
+      "elm/json" = {
+        sha256 = "1g0hafkqf2q633r7ir9wxpb1lnlzskhpsyi0h5bkzj0gl072zfnb";
+        version = "1.0.0";
+      };
+
+      "elm/html" = {
+        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+        version = "1.0.0";
+      };
+
+      "thaterikperson/elm-strftime" = {
+        sha256 = "0fjiqdfrxni7y86l195l7lx9l1zlpc7jiajz0vpvx6wiclkax55j";
+        version = "2.0.2";
+      };
+
+      "elm/browser" = {
+        sha256 = "1apmvyax93nvmagwj00y16zx10kfv640cxpi64xgqbgy7d2wphy4";
+        version = "1.0.0";
+      };
+
+      "elm/core" = {
+        sha256 = "10kr86h4v5h4p0586q406a5wbl8xvr1jyrf6097zp2wb8sv21ylw";
+        version = "1.0.0";
+      };
+
+      "elm/url" = {
+        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+        version = "1.0.0";
+      };
+
+      "ryannhg/date-format" = {
+        sha256 = "070qmq3b5ibqlbsdy4258s50f9a80nxrni43myjkq8dz8sh4b2nd";
+        version = "1.0.0";
+      };
+
+      "elm/http" = {
+        sha256 = "1igmm89ialzrjib1j8xagkxalq1x2gj4l0hfxcd66mpwmvg7psl8";
+        version = "1.0.0";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "NoRedInk/elm-json-decode-pipeline" = {
+        sha256 = "0y25xn0yx1q2xlg1yx1i0hg4xq1yxx6yfa99g272z8162si75hnl";
+        version = "1.0.0";
+      };
+
+      "rtfeldman/elm-iso8601-date-strings" = {
+        sha256 = "1n4zhm00m37fgy4vb6cayplm4yz6k78ijbmqajrqfnbsyc26p8wr";
+        version = "1.1.2";
+      };
+
+      "elm/regex" = {
+        sha256 = "0lijsp50w7n1n57mjg6clpn9phly8vvs07h0qh2rqcs0f1jqvsa2";
+        version = "1.0.0";
+      };
+
+      "elm/parser" = {
+        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+        version = "1.1.0";
+      };
+
+      "elm/virtual-dom" = {
+        sha256 = "0hm8g92h7z39km325dlnhk8n00nlyjkqp3r3jppr37k2k13md6aq";
+        version = "1.0.0";
+      };
+}

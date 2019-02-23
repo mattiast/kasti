01525735da05a1d67a1afbd9c293361c7f5f881c
Matti Astrand <mattiastr@gmail.com>
2019-02-23 08:32:58 +0000

    Use github dependencies for elm generation

--- backend/default.nix
+++ backend/default.nix
@@ -1,6 +1,9 @@
 { nixPkgs ? import <nixpkgs> {}}:
 let
-  kasti = nixPkgs.haskellPackages.callPackage ./kasti.nix { };
+  kasti = nixPkgs.haskellPackages.callPackage ./kasti.nix rec {
+    elm-export = nixPkgs.haskellPackages.callPackage ./elm-export.nix { };
+    servant-elm = nixPkgs.haskellPackages.callPackage ./servant-elm.nix { elm-export = elm-export; };
+  };
   kasti-minimal = nixPkgs.haskell.lib.justStaticExecutables kasti;
 
 in

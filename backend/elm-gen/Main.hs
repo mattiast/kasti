5060923f3b54f81896baf01b2487c8842c6552bd
Matti Astrand <mattiastr@gmail.com>
2020-02-29 03:08:18 +0000

    stuff

--- backend/elm-gen/Main.hs
+++ backend/elm-gen/Main.hs
@@ -5,54 +5,31 @@ module Main where
 import Data.Proxy
 import Types
 import Api
-import Elm
 import Servant.Elm
 import Servant.API.ContentTypes(NoContent)
 import Options.Applicative hiding (header)
+import qualified Data.Text.IO as T
 
-spec :: Spec
-spec =
-  Spec
-    ["Client", "Types"]
-    ([ defElmImports
-    , "import Time exposing (Posix)"
-     , "import Iso8601"
-     , "import Debug exposing (toString)"
-     , "decodeDate = Iso8601.decoder"
-     , "type alias Date = Posix"
-     , "decode = succeed"
 
-    , toElmTypeSource (Proxy :: Proxy Episode)
-    , toElmDecoderSource (Proxy :: Proxy Episode)
-    , toElmEncoderSource (Proxy :: Proxy Episode)
+    -- , toElmTypeSource (Proxy :: Proxy NewEpisode)
+    -- , toElmDecoderSource (Proxy :: Proxy NewEpisode)
+    -- , toElmEncoderSource (Proxy :: Proxy NewEpisode)
 
-    , toElmTypeSource (Proxy :: Proxy EpisodeUrl)
-    , toElmDecoderSource (Proxy :: Proxy EpisodeUrl)
-    , toElmEncoderSource (Proxy :: Proxy EpisodeUrl)
+    -- , toElmTypeSource (Proxy :: Proxy NoContent)
 
-    , toElmTypeSource (Proxy :: Proxy ProgressMsg)
-    , toElmDecoderSource (Proxy :: Proxy ProgressMsg)
-    , toElmEncoderSource (Proxy :: Proxy ProgressMsg)
-
-    , toElmTypeSource (Proxy :: Proxy FeedInfo)
-    , toElmDecoderSource (Proxy :: Proxy FeedInfo)
-    , toElmEncoderSource (Proxy :: Proxy FeedInfo)
-
-    , toElmTypeSource (Proxy :: Proxy ProgressInfo)
-    , toElmDecoderSource (Proxy :: Proxy ProgressInfo)
-    , toElmEncoderSource (Proxy :: Proxy ProgressInfo)
-
-    , toElmTypeSource (Proxy :: Proxy NewEpisode)
-    , toElmDecoderSource (Proxy :: Proxy NewEpisode)
-    , toElmEncoderSource (Proxy :: Proxy NewEpisode)
-
-    , toElmTypeSource (Proxy :: Proxy NoContent)
-
-    ] ++ generateElmForAPI (Proxy :: Proxy Api))
 
 main :: IO ()
 main = do
     let args = argument str (metavar "SRCDIR")
     (srcPath :: FilePath) <- execParser $ info args fullDesc
 
-    specsToDir [spec] srcPath
+    generateElmModule
+        ["Client", "Types"]
+        ""
+        srcPath
+        [ DefineElm (Proxy :: Proxy EStuff)
+        , DefineElm (Proxy :: Proxy Episode)
+        , DefineElm (Proxy :: Proxy FStuff)
+        , DefineElm (Proxy :: Proxy NewEpisode)
+        ]
+        (Proxy :: Proxy Api)
